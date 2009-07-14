79310eda4da9e7e47941e3f6eebe6c9ba3f7d0a0
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-07-14 17:59:02 +0900

    initial commit

---
+++ lib/XMLRPC/Fluent.pm
@@ -0,0 +1,33 @@
+package XMLRPC::Fluent;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+XMLRPC::Fluent -
+
+=head1 SYNOPSIS
+
+  use XMLRPC::Fluent;
+
+=head1 DESCRIPTION
+
+XMLRPC::Fluent is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom  slkjfd gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
